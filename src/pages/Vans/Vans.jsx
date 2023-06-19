a9c58d765fb843f78f7fc8800ecee0afdbd4dc71
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-06-18 21:39:50 -0300

    adding filter buttons to vans page

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,17 +1,13 @@
 import "../../styles/Vans.css";
 import React, { useState, useEffect } from "react";
 import "../../server";
-import { Link , useSearchParams} from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { type } from "@testing-library/user-event/dist/type";
 
 function Vans() {
-
-
-
-  const [searchParams, setSearchParams]  = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const typeFilter = searchParams.get("type");
   console.log(typeFilter);
-
 
   const [vans, setVans] = useState([]);
 
@@ -22,42 +18,47 @@ function Vans() {
   }, []);
 
   const displayElements = typeFilter
-  ? vans.filter(van => van.type.toLowerCase() === typeFilter)
-  : vans;
+    ? vans.filter((van) => van.type.toLowerCase() === typeFilter)
+    : vans;
 
   const vanElements = displayElements
-  // .slice(0, 3) // Take only the first three vans
-  .map((van) => (
-    
-    <Link to={`/vans/${van.id}`} key={van.id} className="vans-image">
-      <img src={van.imageUrl} alt="van" />
-      <div className="van-details">
-        <h3 className="vans-h3">{van.name}</h3>
-        <p className="vans-paragrath">
-          ${van.price}
-          <span>/day</span>
-        </p>
-      </div>
-      <div
-        className={`div-type ${
-          van.type === "simple"
-            ? "green"
-            : van.type === "rugged"
-            ? "orange"
-            : "gray"
-        }`}
-      >
-        {van.type}
-      </div>
-    </Link>
-  ));
-
- 
+    // .slice(0, 3) // Take only the first three vans
+    .map((van) => (
+      <Link to={`/vans/${van.id}`} key={van.id} className="vans-image">
+        <img src={van.imageUrl} alt="van" />
+        <div className="van-details">
+          <h3 className="vans-h3">{van.name}</h3>
+          <p className="vans-paragrath">
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <div
+          className={`div-type ${
+            van.type === "simple"
+              ? "green"
+              : van.type === "rugged"
+              ? "orange"
+              : "gray"
+          }`}
+        >
+          {van.type}
+        </div>
+      </Link>
+    ));
 
   return (
     <div className="Vans">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <Link  className="buttons-filter" to="?type=simple">Simple</Link>
+        <Link className="buttons-filter"  to="?type=rugged">Rugged</Link>
+        <Link  className="buttons-filter" to="?type=luxury">Luxury</Link>
+        <Link className="buttons-filter"  to=".">Clear</Link>
+      </div>
+      <div className="Vans">
       {vanElements}
+      </div>
     </div>
   );
 }
